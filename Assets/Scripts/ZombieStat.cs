a99163e299de8b22e64c40b86102712fe243374a
onegood07 <one_good07@naver.com>
2025-10-29 21:55:50 +0900

    FR-031/feat/#3 : 좀비 이동 속도를 히어로보다 빠르게 설정

--- Assets/Scripts/ZombieStat.cs
+++ Assets/Scripts/ZombieStat.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 public class ZombieStat : MonoBehaviour
 {
     int hp = 1000;
-    int speed = 1000;
+    int speed;
     int power = 1000;
+    public Transform player;
+    private HeroStat heroStat;
   
     void Start()
     {
-        
+        heroStat = player.GetComponent<HeroStat>();
+        if (heroStat != null) speed = (int)(heroStat.speed * 1.1f);
+        else speed = 1000;
     }
 
     void Update()
