18fe6ceb7ab9620c76fc2abb046b1bc0172efcfb
WIN-ORE868LNGCP\dmsdu <dmsdud0028@gmail.com>
2025-10-30 00:55:39 +0900

    FR-040/feat/#4 : new InputSystem을 적용한 수직/수평 이동으로 수정

--- Assets/Scripts/HeroMoveControl.cs
+++ Assets/Scripts/HeroMoveControl.cs
@@ -1,22 +1,26 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class HeroMoveControl : MonoBehaviour
 {
     public float moveSpeed = 5f; // 초기 이동 속도
     private Rigidbody2D rb; // 2D 물리 엔진 컴포넌트 참조
-    private Vector2 moveInput;    
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    private Vector2 moveInput;
 
-    void Update()
-    {
-        // 입력 감지 (WASD -> Horizontal/Vertical)
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+    // Input Actions Asset을 public으로 참조
+    public InputActionAsset inputActions;
+    private InputAction moveAction;  
 
-        moveInput = new Vector2(h, v).normalized;
+    void Awake()
+    {
+
+        // Action Map에서 Move 액션 가져오기
+        moveAction = inputActions.FindActionMap("Player")?.FindAction("Move");
+
+        if (moveAction != null) // Aciton 맵 있는지 확인
+            moveAcion.Enable(); // 입력 활성화
+        else
+            Debug.LogError("Move 액션을 찾을 수 없습니다");
+            // Move 액션이 에셋에 없을 경우 콘솔에 에러 메시지 출력
     }
     
     void FixedUpdate()
