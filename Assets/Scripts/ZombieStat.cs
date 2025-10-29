d4a88de7c45bbda3cb794339e09948cafffd7bff
onegood07 <one_good07@naver.com>
2025-10-29 21:44:02 +0900

    FR-031/feat/#3 : 좀비 HP, 이동속도, 공격력 수치 변수 정의

---
+++ Assets/Scripts/ZombieStat.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks.Dataflow;
+using UnityEngine;
+
+public class ZombieStat : MonoBehaviour
+{
+    int hp = 1000;
+    int speed = 1000;
+    int power = 1000;
+
+    int playerSpeed = 1000;
+  
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+       
+    }
+    
+}
