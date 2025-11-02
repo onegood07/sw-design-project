c34b8c23a28fde5d338b1a4763d69304f2a12a37
onegood07 <one_good07@naver.com>
2025-11-03 06:47:16 +0900

    FR-020/feat/#8 : 게임매니저-enum 설정-게임 엔딩, 게임 일차, 낮밤 페이즈

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,29 @@
 using UnityEngine;
 
+// 게임 엔딩
+public enum GameEnding
+{
+    Happy,
+    GameOver,
+    Bad
+}
+
+// 게임 일차
+public enum GameDays
+{
+    FirstDay,
+    SecondDay,
+    ThirdDay,
+    FourthDay
+}
+
+// 낮과 밤 페이즈
+public enum Phase
+{
+    Day,
+    Night
+}
+
 public class GameManager : MonoBehaviour
 {
     void Start()
