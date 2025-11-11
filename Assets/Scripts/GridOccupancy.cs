4c96ce026ac62254d7611cab4f983d10a0f632b1
dongjun1234 <dongjunkim1234@gmail.com>
2025-11-11 23:22:07 +0900

    FR-021/feat/#13 : 맵 수정 및 좀비 끼리 충돌 방지 스크립트 추가

---
+++ Assets/Scripts/GridOccupancy.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public static class GridOccupancy
+{
+    // 타일맵 별로 "어느 셀을 누가 갖고 있나" 기록
+    private static readonly Dictionary<Tilemap, Dictionary<Vector2Int, ZombieMove>> map = new();
+
+    private static Dictionary<Vector2Int, ZombieMove> Get(Tilemap t)
+    {
+        if (!map.TryGetValue(t, out var dict))
+        {
+            dict = new Dictionary<Vector2Int, ZombieMove>();
+            map[t] = dict;
+        }
+        return dict;
+    }
+
+    public static bool TryReserve(Tilemap t, Vector2Int cell, ZombieMove who)
+    {
+        var dict = Get(t);
+        if (dict.TryGetValue(cell, out var owner) && owner != null && owner != who) return false;
+        dict[cell] = who;
+        return true;
+    }
+
+    public static void Release(Tilemap t, Vector2Int cell, ZombieMove who)
+    {
+        var dict = Get(t);
+        if (dict.TryGetValue(cell, out var owner) && owner == who) dict.Remove(cell);
+    }
+
+    public static bool IsOccupied(Tilemap t, Vector2Int cell)
+    {
+        var dict = Get(t);
+        return dict.ContainsKey(cell);
+    }
+}
