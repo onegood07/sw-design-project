17e5c5b31568518410cfc9a7bbe6eeeb5cd0aa9e
dongjun1234 <dongjunkim1234@gmail.com>
2025-11-12 00:30:06 +0900

    FR-021/feat/#13 : 좀비끼리 겹치지 않게 스크립트 추가

--- Assets/Scripts/GridOccupancy.cs
+++ Assets/Scripts/GridOccupancy.cs
@@ -4,7 +4,7 @@ using UnityEngine.Tilemaps;
 
 public static class GridOccupancy
 {
-    // 타일맵 별로 "어느 셀을 누가 갖고 있나" 기록 (★ owner를 UnityEngine.Object로 일반화)
+    // 타일맵 별로 어느 셀을 누가 갖고 있나 기록 
     private static readonly Dictionary<Tilemap, Dictionary<Vector2Int, Object>> map = new();
 
     private static Dictionary<Vector2Int, Object> Get(Tilemap t)
@@ -17,7 +17,7 @@ public static class GridOccupancy
         return dict;
     }
 
-    // ★ 동일 셀에 다른 주인이 있으면 예약 실패
+    // 동일 셀에 다른 주인이 있으면 예약 실패
     public static bool TryReserve(Tilemap t, Vector2Int cell, Object who)
     {
         var dict = Get(t);
@@ -26,7 +26,7 @@ public static class GridOccupancy
         return true;
     }
 
-    // ★ 내가 점유 중일 때만 반납
+    // 내가 점유 중일 때만 반납
     public static void Release(Tilemap t, Vector2Int cell, Object who)
     {
         var dict = Get(t);
