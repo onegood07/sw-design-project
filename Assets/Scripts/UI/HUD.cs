6264ec664a091432acf0e70bca09201f64bd84ff
WIN-ORE868LNGCP\dmsdu <dmsdud0028@gmail.com>
2025-11-17 17:05:35 +0900

    FR-043/feat/#49 : 페이즈에 따른 시계 이미지(아이콘) 변동 추가

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -1,37 +1,49 @@
+using System.Threading;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
-    public enum InfoType { HP, Hunger, Time, Days }
+    public enum InfoType { NowHP, NowHunger, NowPhase, NowDays }
     public InfoType type;
+    public RectTransform TimeClock;
 
     Text myText;
     Slider mySlider;
+    Image myImage;
+    public Sprite Clock_Day;
+    public Sprite Clock_Night;
+
 
     void Awake()
     {
         myText = GetComponent<Text>();
         mySlider = GetComponent<Slider>();
+        myImage = GetComponent<Image>();
     }
 
     void LateUpdate()
     {
         switch (type) {
-            case InfoType.HP:
+            case InfoType.NowHP:
                 float curHP = HeroStat.Instance.hp;
                 float maxHp = HeroStat.Instance.maxHp;
                 mySlider.value = curHP / maxHp;
                 break;
-            case InfoType.Hunger:
+            case InfoType.NowHunger:
                 float curHunger = HeroStat.Instance.hunger;
                 float maxHunger = HeroStat.Instance.maxHunger;
                 mySlider.value = curHunger / maxHunger;
                 break;
-            case InfoType.Time:
-                // 로직 추가 필요
+            case InfoType.NowPhase:
+                Phase curDays = GameManager.Instance.CurrentPhase;
+                if (curDays == Phase.Day)
+                    myImage.sprite = Clock_Day;
+                else
+                    myImage.sprite = Clock_Night;
                 break;
-            case InfoType.Days:
+            case InfoType.NowDays:
                 myText.text = string.Format("Day {0}", (int)GameManager.Instance.CurrentDay + 1);
                 break;
         }
