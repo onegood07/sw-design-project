d3e00fecc566858b5cb973f6c30d9dfb7cd33700
WIN-ORE868LNGCP\dmsdu <dmsdud0028@gmail.com>
2025-11-17 17:06:46 +0900

    FR-043/feat/#49 : 페이즈 변동할 때 시계바늘 위치가 초기화되지 않는 오류 수정

--- Assets/Scripts/UI/ClockHUD.cs
+++ Assets/Scripts/UI/ClockHUD.cs
@@ -5,15 +5,30 @@ public class ClockHUD : MonoBehaviour
 {
     public float Timer = 0f ;
     public RectTransform ClockHand;
+    private Phase prevPhase;
+
+    void Start()
+    {
+        prevPhase = GameManager.Instance.CurrentPhase;
+    }
 
     void Update()
     {
+        Phase curPhase = GameManager.Instance.CurrentPhase;
+        if (curPhase != prevPhase)
+        {
+            Timer = 0f;
+            prevPhase = curPhase;
+        }
+
         Timer += Time.deltaTime;
+
         float maxTime;
         if (GameManager.Instance.CurrentPhase == Phase.Day)
             maxTime = GameManager.Instance.dayDuration;
         else
             maxTime = GameManager.Instance.nightDuration;
+        
         float angle = (Timer / maxTime) * 360f;
         ClockHand.pivot = new Vector2(0.5f, 0.2f);
         ClockHand.localRotation = Quaternion.Euler(0, 0, -angle);
