200e2fe9dfbcf7337399944df39bf3519980221f
JM-220 <jaemoo52@gmail.com>
2025-11-17 20:11:04 +0900

    sword effect

--- Assets/Scripts/HeroInteraction.cs
+++ Assets/Scripts/HeroInteraction.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,7 +9,9 @@ public class HeroInteraction : MonoBehaviour
     // 컴마로 구분해서 더 넣을 수 있음.
     // rayCast의 성능 향상을 위해 interactableLayer를 가진 요소만 충돌요소로 본다.
     private LayerMask interactableLayer; 
-    
+    public GameObject swordEffectPrefab;
+    public float attackCoolTime;
+    private float lastAttackTime;
     void Start()
     {
         interactableLayer = LayerMask.GetMask("Interactable");
@@ -47,9 +50,24 @@ public class HeroInteraction : MonoBehaviour
             IInteractable targetObj = hit.collider.GetComponent<IInteractable>();
             if(targetObj != null)
             {
+                if (hit.collider.CompareTag("Zombie"))
+                {
+                    createEffect(viewDirection);
+                }
                 // Debug.Log($"{targetObj} interaction target");
                 targetObj.OnInteract();
             }
+        }
+    }
+    void createEffect(Vector2 viewDirection)
+    {
+        Vector2 createPosition = transform.position + (Vector3)viewDirection.normalized;
+        GameObject effect = Instantiate(swordEffectPrefab,createPosition,quaternion.identity);
+        SwordEffect effectSetup = effect.GetComponent<SwordEffect>();
+        if(effect != null)
+        {
+            viewDirection = HeroMoveControl.CurrentViewDirection;
+            effectSetup.Setup(viewDirection);
         }
     }
 }
