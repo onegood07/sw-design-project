e9195d64467ce1002e581c666adfab9ee7da1a4b
WIN-ORE868LNGCP\dmsdu <dmsdud0028@gmail.com>
2025-12-12 00:23:16 +0900

    FR-043/fix/#227 : 메인 씬 변경사항, 스크립트 오탈자 수정 사항 업데이트

--- Assets/Scripts/UI/MinimapCameraControl.cs
+++ Assets/Scripts/UI/MinimapCameraControl.cs
@@ -5,7 +5,7 @@ public class MinimapCameraControl : MonoBehaviour
     public Camera minimapCamera;   // 미니맵 카메라 연결
     public float zoomStep = 5f;    // 한 번에 확대/축소되는 크기
     public float minZoom = 15f;     // 최소 확대
-    public float maxZoom = 30f;    // 최대 축소
+    public float maxZoom = 40f;    // 최대 축소
 
     // 버튼 클릭용
     public void ZoomIn()
