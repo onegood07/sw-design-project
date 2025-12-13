dda1e3ce7dd70344151c4e6f6426720e9885688e
WIN-ORE868LNGCP\dmsdu <dmsdud0028@gmail.com>
2025-12-14 01:56:35 +0900

    FR-004/feat/#233 : 엔딩 컷씬에 타이핑 효과음 추가

--- Assets/Scripts/CutScean/EndCutScenManager.cs
+++ Assets/Scripts/CutScean/EndCutScenManager.cs
@@ -18,6 +18,10 @@ public class EndCutsceneController : MonoBehaviour
     public Vector3 truckSpawnPosition;
     public Vector3 truckSpawnRotation;
 
+    [Header("Dialogue")]
+    public TypingEffect typingEffect;
+
+
 
     GameObject spawnedTruck;
 
@@ -70,17 +74,20 @@ public class EndCutsceneController : MonoBehaviour
 
         Debug.Log($"[Cutscene] spawnedTruck = {spawnedTruck}");
         
+
         if (spawnedTruck != null)
             Debug.Log($"[Cutscene] child count = {spawnedTruck.transform.childCount}");
 
-        dialogueText.text = "드디어 무전기에서 소리가 들리기 시작했다.";
+
+        typingEffect.Play("드디어 무전기에서 소리가 들리기 시작했다.");
         yield return new WaitForSecondsRealtime(3f);
 
-        dialogueText.text = "-아아, 여기는 정부군부대. 생존자는 응답 바랍니다.";
+        typingEffect.Play("-아아, 여기는 정부군부대. 생존자는 응답 바랍니다.");
         yield return new WaitForSecondsRealtime(3f);
 
-        dialogueText.text = "곧 군용 차량이 쉘터 앞으로 도착할 예정이니 준비해 주십시오.";
+        typingEffect.Play("곧 군용 차량이 쉘터 앞으로 도착할 예정이니 준비해 주십시오.");
         yield return new WaitForSecondsRealtime(3f);
+
 
         // 트럭 이동 연출
         Transform mover = spawnedTruck.transform;
@@ -93,10 +100,10 @@ public class EndCutsceneController : MonoBehaviour
 
         yield return StartCoroutine(MoveTruck(mover));
 
-        dialogueText.text = "마침내 우리는 이 도시에서 탈출할 수 있었다.";
+        typingEffect.Play("마침내 우리는 이 도시에서 탈출할 수 있었다.");
         yield return new WaitForSecondsRealtime(3f);
 
-        dialogueText.text = "비록 우리가 향하는 곳이 어디인지, 아무도 알지 못했지만......";
+        typingEffect.Play("비록 우리가 향하는 곳이 어디인지, 아무도 알지 못했지만......");
         yield return new WaitForSecondsRealtime(3f);
 
         EndCutscene();
@@ -110,16 +117,21 @@ public class EndCutsceneController : MonoBehaviour
         DisablePlayerControl();
 
         dialoguePanel.SetActive(true);
-        dialogueText.text = "... 여기 아직 생존자가 있습니다.";
+        FreezeWorld();
+
+        typingEffect.Play("... 여기 아직 생존자가 있습니다.");
         yield return new WaitForSecondsRealtime(3f);
 
-        dialogueText.text = "계속 연락해 봤지만 응답은 없었다.";
+        typingEffect.Play("- ...");
         yield return new WaitForSecondsRealtime(3f);
 
-        dialogueText.text = "결국 나는 이 지옥같은 도시에 혼자 남겨졌다.";
+        typingEffect.Play("계속 연락해 봤지만 응답은 없었다.");
         yield return new WaitForSecondsRealtime(3f);
 
-        dialogueText.text = "혼자서는 절대 살아남을 수 없다.";
+        typingEffect.Play("결국 나는 이 지옥같은 도시에 혼자 남겨졌다.");
+        yield return new WaitForSecondsRealtime(3f);
+
+        typingEffect.Play("혼자서는 절대 살아남을 수 없다.");
         yield return new WaitForSecondsRealtime(3f);
 
         EndCutscene();
